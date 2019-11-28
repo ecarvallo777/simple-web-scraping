5c842185d8d133d650e8da94f3f880bb4e1a31f8
Esteban Carvallo <crewxart@gmail.com>
2019-11-28 01:52:55 -0300

    update

--- request.py
+++ request.py
@@ -1,7 +1,12 @@
 import requests #Import library to request a url
 from bs4 import BeautifulSoup
-url = 'http://condor2.utalca.cl/pls/sgc/matico2_log.parciales?mRutE=0D71C41B4FE4348E&ip=B19C22CA11029E8945E2F348B79D029B&sesion=DE6DA2E18A639B70774C29CF476FAE1A&sistema=MATICO&mPra_codigo=3419'
+url = 'http://condor2.utalca.cl/pls/sgc/matico_log.ficha_alumno?mRutE=0D71C41B4FE4348E&ip=B19C22CA11029E8945E2F348B79D029B&sesion=DE6DA2E18A639B70828FA5834F797ED4&sistema=MATICO'
 
 response = requests.get(url) #Use method of requests to request url
 soup = BeautifulSoup(response.text, 'lxml') #Convert response to an lxml text
-print(soup)
+tables = soup.find_all('td', class_='tabladatosper', align='center')
+print(soup) #This print all html document requested.
+#print(tables) #This print find_all results with tag 'td' and class 'tabladatosper'
+
+#for table in tables:
+    #print(table.text) #Print only text of the tables, one for one with for loop.
