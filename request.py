fc3805bfb3780ec2e8b834ca3c3b6dea1fe232d0
Esteban Carvallo <crewxart@gmail.com>
2019-11-28 16:40:48 -0300

    Print all information

--- request.py
+++ request.py
@@ -1,12 +1,26 @@
 import requests #Import library to request a url
 from bs4 import BeautifulSoup
-url = 'http://condor2.utalca.cl/pls/sgc/matico_log.ficha_alumno?mRutE=0D71C41B4FE4348E&ip=B19C22CA11029E8945E2F348B79D029B&sesion=DE6DA2E18A639B70828FA5834F797ED4&sistema=MATICO'
+url = 'http://condor2.utalca.cl/pls/sgc/matico2_log.parciales?mRutE=0D71C41B4FE4348E&ip=B19C22CA11029E8945E2F348B79D029B&sesion=DE6DA2E18A639B7082A20FE1850BD45F&sistema=MATICO&mPra_codigo=3419'
 
 response = requests.get(url) #Use method of requests to request url
 soup = BeautifulSoup(response.text, 'lxml') #Convert response to an lxml text
-tables = soup.find_all('td', class_='tabladatosper', align='center')
-print(soup) #This print all html document requested.
-#print(tables) #This print find_all results with tag 'td' and class 'tabladatosper'
+items = soup.find('table', width="100%", border="0", cellpadding="1", cellspacing="1")
+tbody= items.find_all('tr')
+count=0
+for i in tbody:
 
-#for table in tables:
-    #print(table.text) #Print only text of the tables, one for one with for loop.
+    
+    if count>0:
+        if count==2 :
+            ramo=i.find('td', class_="tabladatosper", height="19", valign="top").text
+            teacher= i.find('td', align="left", class_="tabladatosper", valign="top").text
+            nota= i.find('td', align="center", class_="tabladatosper", valign="top").text
+            print(ramo+" "+teacher+" "+nota)
+        else:
+            ramo=i.find('a').text
+            teacher= i.find('td', align="left", class_="tabladatosper", valign="top").text
+            nota= i.find('td', align="center", class_="tabladatosper", valign="top").text
+            print(ramo+" "+teacher+" "+nota)
+    count+=1
+#print(items)
+#print(tbody)
